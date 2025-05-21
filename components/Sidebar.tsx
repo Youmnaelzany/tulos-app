1c22fd2d48fc2444be0981e3547683aa05a0ff9d
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-05-22 01:37:22 +0300

    feat: add useOutsideClick hook for handling click outside events

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { X } from "lucide-react";
 import { motion } from "motion/react";
 
 import { headerData } from "@/constants";
+import { useOutsideClick } from "@/hooks/useOutsideClick";
 
 import Logo from "./Logo";
 import SocialMediaLinks from "./SocialMediaLinks";
@@ -16,14 +17,16 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
     <div
-      className={`bg-darkColor/50 hoverEffect fixed inset-y-0 left-0 z-50 w-full shadow-xl ${isOpen ? "translate-x-0" : "-translate-x-full"} `}
+      className={`bg-darkColor/50 hoverEffect fixed inset-y-0 left-0 z-50 w-full cursor-auto shadow-xl ${isOpen ? "translate-x-0" : "-translate-x-full"} `}
     >
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.4, delay: 0.3 }}
+        ref={sidebarRef}
         className="bg-darkColor flex h-full max-w-96 min-w-72 flex-col gap-6 border-r-2 border-r-white p-10 text-white/70"
       >
         {/* Logo & Close Button */}
@@ -34,7 +37,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
           </button>
           {/* Close Button */}
           <button className="" onClick={onClose}>
-            <X className="hoverEffect size-7 text-white hover:text-red-500" />
+            <X className="hoverEffect size-7 cursor-pointer text-white hover:text-red-500" />
           </button>
         </div>
         {/* Navigation Links */}
@@ -44,7 +47,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
               onClick={onClose}
               href={item.href}
               key={item.title}
-              className={`hoverEffect hover:text-white ${pathname === item.href && "text-white"}`}
+              className={`hoverEffect w-24 hover:text-white ${pathname === item.href && "text-white"}`}
             >
               {item.title}
             </Link>
